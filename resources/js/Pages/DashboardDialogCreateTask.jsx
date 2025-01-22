8cbf06cce7b09de69adf845c12c84c4eb59006e7
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2025-01-23 01:35:13 +0700

    feat: add due date

--- resources/js/Pages/DashboardDialogCreateTask.jsx
+++ resources/js/Pages/DashboardDialogCreateTask.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { format } from "date-fns";
 import {
     Dialog,
     DialogContent,
@@ -11,8 +12,18 @@ import {
 import { Button } from "@/components/ui/button";
 import { Label } from "@/Components/ui/label";
 import { Input } from "@/Components/ui/input";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/Components/ui/popover";
+import { cn } from "@/lib/utils";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "@/Components/ui/calendar";
 
 const DashboardDialogCreateTask = () => {
+    const [field, setField] = React.useState(new Date());
+
     return (
         <Dialog>
             <DialogTrigger>
@@ -46,6 +57,44 @@ const DashboardDialogCreateTask = () => {
                             className="col-span-3"
                             type="number"
                         />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="due-date" className="text-right">
+                            Due Date
+                        </Label>
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <Button
+                                    variant={"outline"}
+                                    className={cn(
+                                        "w-[342px] pl-3 text-left font-normal",
+                                        !field && "text-muted-foreground"
+                                    )}
+                                >
+                                    {field ? (
+                                        format(field, "PPP")
+                                    ) : (
+                                        <span>Pick a date</span>
+                                    )}
+                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent
+                                className="w-auto p-0"
+                                align="start"
+                            >
+                                <Calendar
+                                    mode="single"
+                                    selected={field}
+                                    onSelect={setField}
+                                    disabled={(date) =>
+                                        date > new Date() ||
+                                        date < new Date("1900-01-01")
+                                    }
+                                    initialFocus
+                                />
+                            </PopoverContent>
+                        </Popover>
                     </div>
                 </div>
                 <DialogFooter>
