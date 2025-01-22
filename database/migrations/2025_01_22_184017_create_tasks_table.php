8bacf76feaac1c8f6d995ef81ebaa7b88af0b9de
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2025-01-23 01:42:23 +0700

    feat: create task migration

---
+++ database/migrations/2025_01_22_184017_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Foreign key to users table
+            $table->string('task_name');
+            $table->enum('status', ['In Progress', 'Completed'])->default('In Progress');
+            $table->integer('progress')->default(0); // Store percentage (0-100)
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
